2802a4463b1d8d531fcbc0be1ed21e7f469e3bec
gerasimos <gerasimos.xydas@gmail.com>
2018-11-09 15:22:54 +0200

    log methods

--- log.sh
+++ log.sh
@@ -2,6 +2,7 @@
 
 BASH_LIBRARY_LOG=true
 source $BASH_LIBRARIES_PATH/time.sh
+source $BASH_LIBRARIES_PATH/colors.sh
 
 function printDate() {
 	timestamp "+%Y-%m-%d %H:%M:%S"
@@ -9,6 +10,19 @@ function printDate() {
 
 function printTime() {
 	timestamp "+%H:%M:%S"
+}
+
+function loginfo() {
+	# resets the color as well :)
+	echo "$(printDate) ${BASH_SOURCE[1]}:${FUNCNAME[1]} & $*$(color)"
+}
+
+function logerror() {
+	echo "$(color red)$(printDate) ${BASH_SOURCE[1]}:${FUNCNAME[1]} & ERROR: $*$(color)"
+}
+
+function logwarn() {
+	echo "$(color pine)$(printDate) ${BASH_SOURCE[1]}:${FUNCNAME[1]} & WARN: $*$(color)"
 }
 
 #####
